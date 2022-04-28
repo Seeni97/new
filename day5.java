d952a7d27490ec24a268a417ed41f368f3c2223b
Seeni97 <103403744+Seeni97@users.noreply.github.com>
2022-04-28 10:02:07 +0530

    Add files via upload

---
+++ day5.java
@@ -0,0 +1,11 @@
+
+public class day5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=12;
+int b=34;
+
+	}
+
+}
---
+++ nested.java
@@ -0,0 +1,15 @@
+
+public class nested {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public void seenu()
+	{
+		int chennai=250;
+		int covai=100;
+		int salem=570;
+		
+}
+}
